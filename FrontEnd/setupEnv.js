42b974e6c0939933a75b2c85f507ce138b64466d
malang <abdullahbinrafique945@gmail.com>
2024-05-08 16:45:32 +0500

    changed api_endpoint

--- FrontEnd/setupEnv.js
+++ FrontEnd/setupEnv.js
@@ -1,3 +1,8 @@
+// export default {
+//   apiEndpoint: import.meta.env.VITE_API_ENDPOINT || "http://localhost:3000",
+// };
 export default {
-  apiEndpoint: import.meta.env.VITE_API_ENDPOINT || "http://localhost:3000",
+  apiEndpoint:
+    import.meta.env.VITE_API_ENDPOINT ||
+    "https://attendance-2-0-ec76.onrender.com/",
 };
