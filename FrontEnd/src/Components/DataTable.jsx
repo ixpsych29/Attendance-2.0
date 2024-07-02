5065e37f60e2160a8d5266c3cace10f01de9f225
malang <abdullahbinrafique945@gmail.com>
2024-07-02 15:21:09 +0500

    minor ui changes

--- FrontEnd/src/Components/DataTable.jsx
+++ FrontEnd/src/Components/DataTable.jsx
@@ -35,16 +35,16 @@ const rows = [
 
 export default function DataTable() {
   return (
-    <div style={{ height: 400, width: "100%" }}>
+    <div style={{ height: 475, width: "100%" }}>
       <DataGrid
         rows={rows}
         columns={columns}
         initialState={{
           pagination: {
-            paginationModel: { page: 0, pageSize: 5 },
+            paginationModel: { page: 0, pageSize: 7 },
           },
         }}
-        pageSizeOptions={[5, 10]}
+        pageSizeOptions={[7, 10, 20]}
         checkboxSelection
       />
     </div>
