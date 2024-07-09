92f585d588e4dbdbd2cc26b5331db33658dbb343
ahmiii143 <ahmii722@gmail.com>
2024-07-09 12:58:10 +0500

    add the api of lateattendnace api

--- FrontEnd/src/Components/FormatDateTime.jsx
+++ FrontEnd/src/Components/FormatDateTime.jsx
@@ -1,3 +1,29 @@
+// const FormatDateTime = (date) => {
+//   const time = new Date(date);
+//   if (isNaN(time)) {
+//     return { formattedDate: "Invalid Date", formattedTime: "Invalid Time" };
+//   }
+
+//   // Format date
+//   const formattedDate = time.toLocaleDateString("en-US", {
+//     year: "numeric",
+//     month: "long",
+//     day: "2-digit",
+//   });
+
+//   // Format Time
+//   let hours = time.getHours();
+//   const minutes = time.getMinutes();
+//   const period = hours >= 12 ? "PM" : "AM";
+//   hours = hours % 12 || 12;
+
+//   const formattedTime = `${hours}:${minutes
+//     .toString()
+//     .padStart(2, "0")} ${period}`;
+//   return { formattedDate, formattedTime };
+// };
+
+// export default FormatDateTime;
 const FormatDateTime = (date) => {
   const time = new Date(date);
   if (isNaN(time)) {
